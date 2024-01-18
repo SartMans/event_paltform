fd5d5c3503adffdce411cd6b9ce27157c39a0b02
SartMans <sartman@hotmail.com>
2024-01-18 09:52:04 -0500

    route

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -54,6 +54,7 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
  
+ 
   if(eventType==='user.created'){
     const{ id, email_addresses, image_url, first_name, last_name, username}=evt.data;
     const user={
@@ -97,4 +98,3 @@ export async function POST(req: Request) {
  
   return new Response('', { status: 200 })
 }
- 
